326c0814f9de98030f775bf2cbe4e9fab6f39e2c
salty-byte <saltp40@gmail.com>
2022-09-13 22:37:53 +0900

    :+1: チェックボックスの背景色が異なっていたため、テーブル全体の背景色を修正

--- src/main/java/views/LogTable.java
+++ src/main/java/views/LogTable.java
@@ -1,8 +1,10 @@
 package views;
 
 import controllers.CrawlController;
+import java.awt.Component;
 import java.util.Arrays;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 import models.LogEntryKey;
 
 public class LogTable extends JTable {
@@ -15,5 +17,21 @@ public class LogTable extends JTable {
     setRowHeight(20);
     Arrays.stream(LogEntryKey.values())
         .forEach(v -> this.getColumn(v.getDisplayName()).setPreferredWidth(v.getWidth()));
+  }
+
+  @Override
+  public Component prepareRenderer(TableCellRenderer renderer, int rowIndex, int columnIndex) {
+    final var component = super.prepareRenderer(renderer, rowIndex, columnIndex);
+    final var selectedRows = getSelectedRows();
+
+    if (Arrays.stream(selectedRows).anyMatch(r -> r == rowIndex)) {
+      component.setBackground(getSelectionBackground());
+      component.setForeground(getSelectionForeground());
+      return component;
+    }
+
+    component.setForeground(getForeground());
+    component.setBackground(getBackground());
+    return component;
   }
 }
