27f18e38fac123554db0a018f2c6764abedf5cea
salty-byte <saltp40@gmail.com>
2022-09-30 15:55:01 +0900

    :+1: 変更可能な背景色の調整

--- src/main/java/models/ColorType.java
+++ src/main/java/models/ColorType.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 public enum ColorType {
   DEFAULT((byte) 0x0, "デフォルト", null, null),
-  RED((byte) 0x1, "赤", new Color(255, 99, 71), Color.WHITE),
+  RED((byte) 0x1, "赤", new Color(255, 128, 114), Color.BLACK),
   ORANGE((byte) 0x2, "橙", new Color(255, 201, 102), Color.BLACK),
   YELLOW((byte) 0x3, "黄", new Color(255, 255, 102), Color.BLACK),
   GREEN((byte) 0x4, "緑", new Color(128, 255, 128), Color.BLACK),
-  BLUE((byte) 0x5, "青", new Color(112, 140, 224), Color.WHITE),
+  BLUE((byte) 0x5, "青", new Color(112, 140, 224), Color.BLACK),
   PURPLE((byte) 0x6, "紫", new Color(255, 179, 242), Color.BLACK),
-  GRAY((byte) 0x7, "灰", Color.GRAY, Color.BLACK),
+  GRAY((byte) 0x7, "灰", new Color(180, 180, 180), Color.BLACK),
   ;
 
   private final byte value;
